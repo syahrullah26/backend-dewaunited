6377ece2a561825d74e3e25433d7d4a57d555f3a
Muhammad Syahrulah <muhammadsyahrullah26@gmail.com>
2026-01-27 16:04:16 +0700

    add admin panel cors

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,13 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+        'login',
+        'logout',
+        'user'
+    ],
 
     'allowed_methods' => ['*'],
 
@@ -19,6 +25,5 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
-
+    'supports_credentials' => true,
 ];
